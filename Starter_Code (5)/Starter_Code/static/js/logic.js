fb43419a2c7a9b947fc339ea7c7649fa39815eb2
Leigha <lsrussell834@gmail.com>
2023-06-24 14:21:24 -0700

    FFinal map w/ markers, legend still needed

---
+++ Starter_Code (5)/Starter_Code/static/js/logic.js
@@ -0,0 +1,136 @@
+// Store our API endpoint as queryUrl.
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+  // Once we get a response, send the data.features object to the createFeatures function.
+  createFeatures(data.features);
+});
+function createFeatures(earthquakeData) {
+
+  // Define a function that we want to run once for each feature in the features array.
+  // Give each feature a popup that describes the place and time of the earthquake.
+  function onEachFeature(feature, layer) {
+    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p><hr><p>Magnitude: ${feature.properties.mag}</p><hr><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+  }
+  function markerInfo(feature) {
+    return {
+        color: "black",
+        fillColor: setColor(feature.geometry.coordinates[2]),
+        radius: setSize(feature.properties.mag),
+        fillOpacity: 1,
+        weight: 1.5
+    };
+};
+   // This function determines the color of the marker based on the depth of the earthquake.
+    function setColor(depth) {
+        if (depth > 90) {
+        return "#ea2c2c";
+        }
+        if (depth > 70) {
+        return "#ea822c";
+        }
+        if (depth > 50) {
+        return "#ee9c00";
+        }
+        if (depth > 30) {
+        return "#eecc00";
+        }
+        if (depth > 10) {
+        return "#d5470d"
+        } 
+        return "#98ee00";
+    };
+    //Use the function that determines the size of the earthquake marker based on its magnitude.
+    function setSize(magnitude) {
+        if (magnitude === 0) {
+            return 1;
+        }
+        return magnitude * 4;
+        };
+  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+  // Run the onEachFeature function once for each piece of data in the array.
+  let earthquakes = L.geoJSON(earthquakeData, {
+    pointToLayer: function(feature, latlng) {
+        console.log(earthquakeData);
+        return L.circleMarker(latlng);
+    },
+    style: markerInfo,
+    onEachFeature: onEachFeature
+  });
+
+  // Send our earthquakes layer to the createMap function/
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+  // Create the base layers.
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  })
+
+  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  });
+
+  // Create a baseMaps object.
+  let baseMaps = {
+    "Street Map": street,
+    "Topographic Map": topo
+  };
+
+  // Create an overlay object to hold our overlay.
+  let overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load.
+  let myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [street, earthquakes]
+  });
+
+  // Create a layer control.
+  // Pass it our baseMaps and overlayMaps.
+  // Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+  
+//   // Set up the legend.
+//   let legend = L.control({ position: "bottomright" });
+//   legend.onAdd = function(myMap) {
+//     let div = L.DomUtil.create("div", "info legend");
+//     let limits = earthquakes.options.limits;
+//     let colors = earthquakes.options.colors;
+//     let labels = [];
+
+//     // Add the minimum and maximum.
+//     let legendInfo = "<h1>Population with Children<br />(ages 6-17)</h1>" +
+//       "<div class=\"labels\">" +
+//         "<div class=\"min\">" + limits[0] + "</div>" +
+//         "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+//       "</div>";
+
+//     div.innerHTML = legendInfo;
+
+//     limits.forEach(function(limit, index) {
+//       labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+//     });
+
+//     div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+//     return div;
+//   };
+
+//   // Adding the legend to the map
+//   legend.addTo(myMap);
+
+
+ };
+ 
+
+
